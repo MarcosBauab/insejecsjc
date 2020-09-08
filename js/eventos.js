10a15d8034638863717ffa5b8db9b2f84d06c004
Marcos Vinicius Gasparoto Bauab <marcosbauab@gmail.com>
2020-09-08 16:31:03 -0300

    modal quase terminado
    só falta o conteúdo

--- js/eventos.js
+++ js/eventos.js
@@ -1,18 +1,26 @@
-let modal = document.getElementsByClassName('modal')
+let modaltaber = document.getElementById('modalTaber')
 let taber = document.querySelector("#taber")
 taber.addEventListener('click',()=>{
-    for(let i = 0; i< modal.length;i++){
-        modal[i].classList.remove('hide')
-    }
+    modaltaber.classList.remove('hide')   
 })
+let modalLuz = document.getElementById('modalLuz')
 let luz = document.querySelector("#luz")
 luz.addEventListener('click',()=>{
-    window.location.href = "tabernaculos.html"
+    modalLuz.classList.remove('hide')
 })
+let modalg12 = document.getElementById('modal-g12')
+let g12 = document.querySelector("#g12")
+g12.addEventListener('click',()=>{
+    modalg12.classList.remove('hide')
+})
+let fechar = document.querySelectorAll('a#fechar')
+for(let i = 0; i< fechar.length;i++){
+    fechar[i].addEventListener('click', () => {
+        modalLuz.setAttribute('class', 'hide') 
+        modaltaber.setAttribute('class', 'hide')
+        modalg12.setAttribute('class', 'hide')
+    })
+}
 
-const fechar = document.querySelector('.modal .header a')
-fechar.addEventListener('click', () => {
-    for(let i = 0; i< modal.length;i++){
-        modal[i].setAttribute('class', 'hide')
-    }
-})
+
+
