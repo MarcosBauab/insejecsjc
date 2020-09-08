055e612d906640568e5f9d55b953e3ff1d4baf34
Marcos Vinicius Gasparoto Bauab <marcosbauab@gmail.com>
2020-09-08 15:51:18 -0300

    quase acabando o modal

--- js/eventos.js
+++ js/eventos.js
@@ -1,8 +1,18 @@
+let modal = document.getElementsByClassName('modal')
 let taber = document.querySelector("#taber")
 taber.addEventListener('click',()=>{
-    window.location.href = "tabernaculos.html"
+    for(let i = 0; i< modal.length;i++){
+        modal[i].classList.remove('hide')
+    }
 })
 let luz = document.querySelector("#luz")
 luz.addEventListener('click',()=>{
     window.location.href = "tabernaculos.html"
+})
+
+const fechar = document.querySelector('.modal .header a')
+fechar.addEventListener('click', () => {
+    for(let i = 0; i< modal.length;i++){
+        modal[i].setAttribute('class', 'hide')
+    }
 })
