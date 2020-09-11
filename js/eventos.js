c2274102cc78430a0f914e798cceececff24dbc8
Marcos Vinicius Gasparoto Bauab <marcosbauab@gmail.com>
2020-09-11 16:48:55 -0300

    Modal otimizado
    Menos código e mais coisa

--- js/eventos.js
+++ js/eventos.js
@@ -1,4 +1,23 @@
-let modaltaber = document.getElementById('modalTaber')
+let classModal = document.getElementsByClassName("modal")
+let arrModal = Array.from(classModal)
+
+let fechar = document.querySelectorAll('a#fechar')
+for(let i = 0; i< fechar.length;i++){
+    fechar[i].addEventListener('click', () => {
+        arrModal[i].setAttribute('class', 'hide')
+        arrModal[i].classList.add('modal')
+    })
+}
+
+let clicaveis = document.querySelectorAll("button[clicable]")
+let arrClica = Array.from(clicaveis)
+for(let i = 0; i< arrClica.length; i++){
+    arrClica[i].addEventListener('click', function(){
+        arrModal[i].classList.remove('hide')
+    })
+}
+//Código não otimizado:
+/*let modaltaber = document.getElementById('modalTaber')
 let taber = document.querySelector("#taber")
 taber.addEventListener('click',()=>{
     modaltaber.classList.remove('hide')   
@@ -12,18 +31,4 @@ let modalg12 = document.getElementById('modal-g12')
 let g12 = document.querySelector("#g12")
 g12.addEventListener('click',()=>{
     modalg12.classList.remove('hide')
-})
-let fechar = document.querySelectorAll('a#fechar')
-for(let i = 0; i< fechar.length;i++){
-    fechar[i].addEventListener('click', () => {
-        modalLuz.setAttribute('class', 'hide') 
-        modaltaber.setAttribute('class', 'hide')
-        modalg12.setAttribute('class', 'hide')
-        modalLuz.classList.add('modal') 
-        modaltaber.classList.add('modal')
-        modalg12.classList.add('modal')
-    })
-}
-
-
-
+})*/
