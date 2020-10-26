fb3ab18651b64b7b0312f44ff0cea517618a4ea2
k <39273657+hid3aki@users.noreply.github.com>
2020-10-26 17:52:56 -0200

    Ajuste para funcionar (Enable errors to see errors)
    https://stackoverflow.com/questions/23573910/php-web-page-not-loading

--- config.php
+++ config.php
@@ -1,4 +1,6 @@
 <?php
     define('INCLUDE_PATH','http://localhost/insejecsjc/');
     define('TITULO','Nossa Igreja - INSEJEC SJC');
+    ini_set('error_reporting', E_ALL);
+    ini_set('display_errors', true);
 ?>
